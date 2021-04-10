a26b9d0c812ab38080529e66eb27d2480e6acea3
Konrad Komisarczyk <32682692+konrad-komisarczyk@users.noreply.github.com>
2021-04-10 11:18:21 +0200

    raport v2 (#15)
    * dodanie wszystkich plików
    
    * poprawki + dodanie prezentacji

---
+++ Admin123/raport_v2/codes/libraries.R
@@ -0,0 +1,19 @@
+# Basics
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(stringr)
+
+# Models
+library(xgboost)
+library(dbscan)
+#devtools::install_github('ModelOriented/treeshap')
+library(treeshap)
+#library(caret)
+
+# EDA
+library(DataExplorer)
+
+library(patchwork)
+library(gridExtra)
+
